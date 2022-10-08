4d497a18b50cedb1cee8eef89c80457358298c10
Horacio <balderashoracio@hotmail.com>
2022-10-08 18:12:06 -0500

    Se conecto a la base de datos

--- social_django/social_django/settings.py
+++ social_django/social_django/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'social_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'AgendaControlMedico',
+       'USER': 'postgres',
+       'PASSWORD': 'acm',
+       'HOST': '',
+       'PORT': '2000',
     }
 }
 
